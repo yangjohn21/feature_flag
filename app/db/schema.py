1055192950ccc8be74dfff954d669b734734e9f8
yangjohn21 <john3856john@gmail.com>
2026-06-15 13:50:18 -0700

    debug

--- app/db/schema.py
+++ app/db/schema.py
@@ -1,5 +1,7 @@
-from sqlalchemy import String, create_engine
+from sqlalchemy import String, create_engine, event
 import os
+import re
+import logging
 from sqlalchemy import Boolean, ForeignKey, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
@@ -13,6 +15,24 @@ connect_args = {"application_name": config.app_name}
 
 engine = create_engine(config.db_url, echo=echo, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Log the masked DB URL for each executed query so we can identify which DB handled it.
+_engine_logger = logging.getLogger("sqlalchemy.engine.Engine")
+
+def _mask_db_url(url: str) -> str:
+    try:
+        return re.sub(r"(://[^:/]+:)([^@]+)(@)", r"\1***\3", url)
+    except Exception:
+        return "<masked>"
+
+
+@event.listens_for(engine, "before_cursor_execute")
+def _log_db_url_before_execute(conn, cursor, statement, parameters, context, executemany):
+    try:
+        url = str(conn.engine.url)
+    except Exception:
+        url = config.db_url
+    _engine_logger.info("DB URL for query: %s", _mask_db_url(url))
 
 
 class Base(DeclarativeBase):
